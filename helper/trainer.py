Fixed training on ensemble_task_2, removed Softmax function from models, changed non-linearity on Task 2.

helper/trainer.py
@@ -1,10 +1,13 @@
 import numpy as np
 import torch
+from models.CNN_task_2 import ensemble_task_2
 from collections import OrderedDict
 from torch.utils.data import DataLoader
 
-def train_model(model : torch.nn.Module | list[torch.nn.Module], train_loader : DataLoader, val_loader : DataLoader, loss : torch.nn.modules.loss._Loss, optimizer : torch.optim.Optimizer,
-                 device : torch.device, max_epochs : int, epoch_print_ratio : int = 10, early_stopping : bool = True, patience : int = 100) -> tuple[OrderedDict, list[float], list[float], list[float], list[float]]:
+def train_model(model : torch.nn.Module | ensemble_task_2, train_loader : DataLoader, val_loader : DataLoader,
+                loss : torch.nn.modules.loss._Loss, device : torch.device, max_epochs : int, 
+                learning_rate : float = 0.001, epoch_print_ratio : int = 10, early_stopping : bool = True,
+                patience : int = 100, is_adam : bool = True, momentum : float = 0.0) -> tuple[OrderedDict, list[float], list[float], list[float], list[float]]:
     """
         This function trains the passed model up to the number of epochs specified and then returns *the state_dict()* of the model with the lowest loss on the validation set, as well as the training and validation losses and accuracies.
     
@@ -20,53 +23,35 @@ def train_model(model : torch.nn.Module | list[torch.nn.Module], train_loader : 
 
     epochs_without_improvement = 0
 
-    # Training loop
+    if isinstance(model, ensemble_task_2):
+        optimizer = [torch.optim.Adam(model_voter.parameters(), lr=learning_rate) if is_adam else torch.optim.SGD(model_voter.parameters(), lr=learning_rate, momentum=momentum) for model_voter in model.models]
+    else:
+        optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate) if is_adam else torch.optim.SGD(model.parameters(), lr=learning_rate, momentum=momentum)
+
     for epoch in range(max_epochs):
+        # Training
         if epoch % epoch_print == 0:
             print(f"Epoch {epoch + 1} of {max_epochs}")
-        model.train()
-        train_loss = 0
-        correct = 0
-        total = 0
-        for x, y in iter(train_loader):
-            x = x.to(device)
-            y = y.to(device)
-            optimizer.zero_grad()
-            y_pred = model(x)
-            l = loss(y_pred, y)
-            l.backward()
-            optimizer.step()
-            train_loss += l.item()
-            # Calculate accuracy
-            _, predicted = torch.max(y_pred.data, 1)
-            total += y.size(0)
-            correct += (predicted == y).sum().item()
-        train_loss /= len(train_loader)
+
+        if isinstance(model, ensemble_task_2):
+            for n, model_voter in enumerate(model.models):
+                _train_individual_model(model_voter, train_loader, optimizer[n], loss, device)
+        else:
+            _train_individual_model(model, train_loader, optimizer, loss, device)
+
+        train_loss, accuracy = _calculate_loss_accuracy(model, train_loader, loss, device)
         train_losses.append(train_loss)
-        accuracy = correct / total
         train_accuracies.append(accuracy)
+        
         if epoch % epoch_print == 0:
             print(f"Training loss: {train_loss}")
             print(f"Training accuracy: {accuracy*100}%")
 
-        # Validation loop
+        # Validation
         model.eval()
-        val_loss = 0
-        correct = 0
-        total = 0
         with torch.no_grad():
-            for x_val, y_val in iter(val_loader):
-                x_val = x_val.to(device)
-                y_val = y_val.to(device)
-                y_pred_val = model(x_val)
-                l_val = loss(y_pred_val, y_val)
-                val_loss += l_val.item()
-                _, predicted = torch.max(y_pred_val.data, 1)
-                total += y_val.size(0)
-                correct += (predicted == y_val).sum().item()
-            val_loss /= len(val_loader)
+            val_loss, val_acc = _calculate_loss_accuracy(model, val_loader, loss, device)
             val_losses.append(val_loss)
-            val_acc = correct / total
             val_accuracies.append(val_acc)
             if epoch % epoch_print == 0:
                 print(f"Validation loss: {val_loss}")
@@ -100,3 +85,37 @@ def test_model(model : torch.nn.Module, test_loader : DataLoader, device : torch
 
     test_accuracy = 100 * correct / total
     return test_accuracy
+
+def _train_individual_model(model : torch.nn.Module, train_loader : DataLoader, optimizer : torch.optim.Optimizer,
+                            loss : torch.nn.modules.loss._Loss, device : torch.device) -> None:
+    model.train()
+    train_loss = 0
+    for x, y in iter(train_loader):
+        x = x.to(device)
+        y = y.to(device)
+        optimizer.zero_grad()
+        y_pred = model(x)
+        l = loss(y_pred, y)
+        l.backward()
+        optimizer.step()
+        train_loss += l.item()
+    return
+
+def _calculate_loss_accuracy(model : torch.nn.Module, loader : DataLoader, loss : torch.nn.modules.loss._Loss, device : torch.device) -> tuple[float, float]:
+    model.eval()
+    loss_ = 0
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for x, y in iter(loader):
+            x = x.to(device)
+            y = y.to(device)
+            y_pred = model(x)
+            l = loss(y_pred, y)
+            loss_ += l.item()
+            _, predicted = torch.max(y_pred.data, 1)
+            total += y.size(0)
+            correct += (predicted == y).sum().item()
+    loss_ /= len(loader)
+    accuracy = correct / total
+    return (loss_, accuracy)

main.py
@@ -14,8 +14,8 @@ if __name__ == "__main__":
     torch.backends.cudnn.benchmark = False
 
     print("Task 1:")
-    tasks.task_1(MAX_EPOCHS=1500)
+    tasks.task_1(MAX_EPOCHS=300,PATIENCE=30)
     print("Task 2:")
-    tasks.task_2(NUMBER_OF_MODELS=10,CONV_KERNEL_SIZES=(7,7,5),PADDING=(3,3,2),DROPOUT=0.25,MAX_EPOCHS=2000)
+    tasks.task_2(NUMBER_OF_MODELS=10,CONV_KERNEL_SIZES=(7,7,7),PADDING=(3,3,3),DROPOUT=0.25,MAX_EPOCHS=300,PATIENCE=50,LEARNING_RATE=0.001,BETA=1.5,CAP=10.0)
     print("Task 3:")
-    tasks.task_3(MAX_EPOCHS=300,PATIENCE=100,EPOCHS_SVM=3)
+    tasks.task_3(MAX_EPOCHS=300,PATIENCE=50,EPOCHS_SVM=3)

models/CNN_task_1.py
@@ -34,8 +34,7 @@ class CNN_task_1(nn.Module):
             nn.Conv2d(in_channels=16, out_channels=32, kernel_size=self.conv_kernel_sizes, stride=1, padding=self.padding),
             nn.ReLU(),
             nn.Flatten(),
-            nn.Linear(in_features=self.last_input_size, out_features=15),
-            nn.Softmax(dim=1)
+            nn.Linear(in_features=self.last_input_size, out_features=15)
         )
 
         # Initialize weights:

models/CNN_task_2.py
@@ -1,9 +1,11 @@
+import torch
 import torch.nn as nn
 
 class CNN_task_2(nn.Module):
 
     def __init__(self, conv_kernel_sizes : tuple[int, int, int], dropout : float, batch_normalization = True, input_height : int = 64, kaiming_normal_ : bool = True,
-                 input_width : int = 64, mean_initialization : float = 0.0, std_initialization : float = 0.01, padding : tuple[int, int, int] = (1, 1, 1)):
+                 input_width : int = 64, mean_initialization : float = 0.0, std_initialization : float = 0.01, padding : tuple[int, int, int] = (1, 1, 1),
+                 beta : float = 1.0, cap : float = 10.0):
         
         super().__init__()
 
@@ -19,6 +21,9 @@ class CNN_task_2(nn.Module):
         self.conv_kernel_sizes = conv_kernel_sizes
         self.dropout = dropout
 
+        self.beta = beta
+        self.cap = cap
+
         # The amount of features in the last linear layer is calculated as follows (with k_n = (kernel_size_n/2-padding_n)):
         # (height/2-k_1-2*k_2)/2-2*k_3 x (width/2-k_1-2*k_2)/2-2*k_3 x 32
         # Ideally, all k_n should be equal to 0 to get height/4 x width/4 x 32.
@@ -29,19 +34,18 @@ class CNN_task_2(nn.Module):
         # Layers:
         self.layers_list = [
             nn.Conv2d(in_channels=1, out_channels=8, kernel_size=self.conv_kernel_sizes[0], stride=1, padding=self.padding[0]),
-            nn.ReLU(),
+            SwishC(beta=self.beta, cap=self.cap),
             nn.Dropout(p=self.dropout),
             nn.MaxPool2d(kernel_size=2, stride=2),
             nn.Conv2d(in_channels=8, out_channels=16, kernel_size=self.conv_kernel_sizes[1], stride=1, padding=self.padding[1]),
-            nn.ReLU(),
+            SwishC(beta=self.beta, cap=self.cap),
             nn.Dropout(p=self.dropout),
             nn.MaxPool2d(kernel_size=2, stride=2),
             nn.Conv2d(in_channels=16, out_channels=32, kernel_size=self.conv_kernel_sizes[2], stride=1, padding=self.padding[2]),
-            nn.ReLU(),
+            SwishC(beta=self.beta, cap=self.cap),
             nn.Dropout(p=self.dropout),
             nn.Flatten(),
-            nn.Linear(in_features=self.last_input_size, out_features=15),
-            nn.Softmax(dim=1)
+            nn.Linear(in_features=self.last_input_size, out_features=15)
         ]
 
         # Add batch normalization layers if required:
@@ -75,7 +79,8 @@ class ensemble_task_2(nn.Module):
 
     def __init__(self, number_of_models : int, conv_kernel_sizes : tuple[int, int, int], dropout : float, batch_normalization = True, 
                  input_height : int = 64, kaiming_normal_ : bool = True, input_width : int = 64, 
-                 mean_initialization : float = 0.0, std_initialization : float = 0.01, padding : tuple[int, int, int] = (1, 1, 1)):
+                 mean_initialization : float = 0.0, std_initialization : float = 0.01, padding : tuple[int, int, int] = (1, 1, 1), 
+                 beta : float = 1.0, cap : float = 10.0):
         super().__init__()
 
         self.models = nn.ModuleList([
@@ -88,9 +93,21 @@ class ensemble_task_2(nn.Module):
                 input_width=input_width, 
                 mean_initialization=mean_initialization, 
                 std_initialization=std_initialization, 
-                padding=padding
+                padding=padding,
+                beta=beta,
+                cap=cap
             ) for _ in range(number_of_models)])
 
     def forward(self, x):
         outputs = [model(x) for model in self.models]
         return sum(outputs) / len(outputs)
+    
+# Capped Swish activation function:
+class SwishC(nn.Module):
+    def __init__(self, beta=1.0, cap=10.0):
+        super().__init__()
+        self.beta = beta
+        self.cap = torch.tensor(cap)
+
+    def forward(self, x):
+        return torch.where(x < self.cap, x * torch.sigmoid(self.beta * x), self.cap)

tasks/tasks.py
@@ -19,11 +19,11 @@ _SPLIT_RATIO = 0.85
 _BATCH_SIZE = 32
 
 # Task 1
-_LEARNING_RATE = 1e-3
+_LEARNING_RATE = 1e-3   
 _MOMENTUM = 0.9
 _MAX_EPOCHS = 1000
 _EARLY_STOPPING = True
-_PATIENCE = 150
+_PATIENCE = 30
 
 # Task 2
 _CONV_KERNEL_SIZES = (3, 3, 3)
@@ -31,6 +31,8 @@ _PADDING = (1, 1, 1)
 _BATCH_NORMALIZATION = True
 _DROPOUT = 0.3
 _NUMBER_OF_MODELS = 5
+_BETA = 1.0
+_CAP = 10.0
 
 # Task 3
 _EPOCHS_SVM = 3
@@ -60,21 +62,22 @@ def task_1(SPLIT_RATIO=_SPLIT_RATIO, BATCH_SIZE=_BATCH_SIZE, LEARNING_RATE=_LEAR
     model = CNN_task_1().to(device)
 
     loss = torch.nn.CrossEntropyLoss()
-    optimizer = torch.optim.SGD(model.parameters(), lr=LEARNING_RATE, momentum=MOMENTUM)
 
     # Train and save model and training and validation losses and accuracies
     best_model, train_losses, val_losses, train_accuracies, val_accuracies = \
         train_model(
-            model,
-            train_loader, 
-            val_loader, 
-            loss, 
-            optimizer, 
-            device, 
-            MAX_EPOCHS, 
+            model=model,
+            train_loader=train_loader, 
+            val_loader=val_loader, 
+            loss=loss, 
+            device=device, 
+            max_epochs=MAX_EPOCHS, 
+            learning_rate=LEARNING_RATE,
             epoch_print_ratio=EPOCH_PRINT_RATIO, 
             early_stopping=EARLY_STOPPING, 
-            patience=PATIENCE
+            patience=PATIENCE,
+            is_adam=False,
+            momentum=MOMENTUM
         )
 
     # Load best model
@@ -108,7 +111,8 @@ def task_1(SPLIT_RATIO=_SPLIT_RATIO, BATCH_SIZE=_BATCH_SIZE, LEARNING_RATE=_LEAR
 
 def task_2(SPLIT_RATIO=_SPLIT_RATIO, BATCH_SIZE=_BATCH_SIZE, LEARNING_RATE=_LEARNING_RATE, MOMENTUM=_MOMENTUM, MAX_EPOCHS=_MAX_EPOCHS,
             EPOCH_PRINT_RATIO=_EPOCH_PRINT_RATIO, CONV_KERNEL_SIZES=_CONV_KERNEL_SIZES, DROPOUT=_DROPOUT, PADDING=_PADDING,
-            EARLY_STOPPING=_EARLY_STOPPING, PATIENCE=_PATIENCE, BATCH_NORMALIZATION=_BATCH_NORMALIZATION, NUMBER_OF_MODELS=_NUMBER_OF_MODELS):
+            EARLY_STOPPING=_EARLY_STOPPING, PATIENCE=_PATIENCE, BATCH_NORMALIZATION=_BATCH_NORMALIZATION, NUMBER_OF_MODELS=_NUMBER_OF_MODELS,
+            BETA=_BETA, CAP=_CAP):
 
     # Anisotropic scaling and conversion to tensor.
     transform = transforms.Compose([
@@ -120,7 +124,7 @@ def task_2(SPLIT_RATIO=_SPLIT_RATIO, BATCH_SIZE=_BATCH_SIZE, LEARNING_RATE=_LEAR
 
     data_augmentation_transform = transforms.Compose([
         transforms.RandomHorizontalFlip(),                                                      # Randomly flip image horizontally
-        transforms.RandomRotation(degrees=10),                                                  # Randomly rotate image
+        transforms.RandomRotation(degrees=20),                                                  # Randomly rotate image
         transforms.RandomChoice([                                                               # Randomly choose whether apply anisotropic rescaling or random cropping (2 to 1 ratio).  
             transforms.Resize((64, 64)),
             transforms.Resize((64, 64)),
@@ -131,7 +135,7 @@ def task_2(SPLIT_RATIO=_SPLIT_RATIO, BATCH_SIZE=_BATCH_SIZE, LEARNING_RATE=_LEAR
         ]),
         transforms.Grayscale(),
         transforms.ToTensor(),
-        transforms.Lambda(lambda x: torch.clamp(x + 0.01*torch.randn_like(x),min=0.,max=1.)),   # Add random noise to image.
+        transforms.Lambda(lambda x: torch.clamp(x + 0.02*torch.randn_like(x),min=0.,max=1.)),   # Add random noise to image.
         transforms.Normalize(mean=[0.5], std=[0.5])                                             # Normalize image to [-1, 1]
     ])
 
@@ -148,22 +152,23 @@ def task_2(SPLIT_RATIO=_SPLIT_RATIO, BATCH_SIZE=_BATCH_SIZE, LEARNING_RATE=_LEAR
         conv_kernel_sizes=CONV_KERNEL_SIZES, 
         dropout=DROPOUT, 
         padding=PADDING, 
-        batch_normalization=BATCH_NORMALIZATION
+        batch_normalization=BATCH_NORMALIZATION,
+        beta=BETA,
+        cap=CAP
     ).to(device)
 
     loss = torch.nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
 
     # Train and save model and training and validation losses and accuracies
     best_model, train_losses, val_losses, train_accuracies, val_accuracies = \
         train_model(
-            model, 
-            train_loader, 
-            val_loader, 
-            loss, 
-            optimizer, 
-            device, 
-            MAX_EPOCHS, 
+            model=model, 
+            train_loader=train_loader, 
+            val_loader=val_loader, 
+            loss=loss,
+            device=device, 
+            max_epochs=MAX_EPOCHS, 
+            learning_rate=LEARNING_RATE,
             epoch_print_ratio=EPOCH_PRINT_RATIO,
             early_stopping=EARLY_STOPPING,
             patience=PATIENCE
@@ -241,18 +246,17 @@ def task_3(SPLIT_RATIO=_SPLIT_RATIO, BATCH_SIZE=_BATCH_SIZE, LEARNING_RATE=_LEAR
     model = CNN_task_3_classifier().to(device)
 
     loss = torch.nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
 
     # Train and save model and training and validation losses and accuracies
     best_model, train_losses, val_losses, train_accuracies, val_accuracies = \
         train_model(
-            model, 
-            train_loader, 
-            val_loader, 
-            loss, 
-            optimizer, 
-            device, 
-            MAX_EPOCHS, 
+            model=model, 
+            train_loader=train_loader, 
+            val_loader=val_loader, 
+            loss=loss, 
+            device=device, 
+            max_epochs=MAX_EPOCHS, 
+            learning_rate=LEARNING_RATE,
             epoch_print_ratio=EPOCH_PRINT_RATIO,
             early_stopping=EARLY_STOPPING,
             patience=PATIENCE